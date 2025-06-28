7e8503c25c46455bed15e6b3284b34b4e1a10e7a
aybhghggf <ayoubehamadi5@gmail.com>
2025-06-29 00:33:20 +0100

    Add success and error alerts to Login and Register pages

--- lmarketplace-lik/resources/views/Register.blade.php
+++ lmarketplace-lik/resources/views/Register.blade.php
@@ -1,1 +1,14 @@
+<!-- Success Alert -->
+@if(session('success'))
+    <x-alert-succes>
+        {{ session('success') }}
+    </x-alert-succes>
+@endif
+
+@if(session('error'))
+    <x-fail-alert>
+        {{ session('error') }}
+    </x-fail-alert>
+@endif
+
 <x-register/>
