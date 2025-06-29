3fd4ab53e4b50667033a91380af4c8e22124942d
aybhghggf <ayoubehamadi5@gmail.com>
2025-06-29 14:52:24 +0100

    update flash message

--- lmarketplace-lik/resources/views/components/fail-alert.blade.php
+++ lmarketplace-lik/resources/views/components/fail-alert.blade.php
@@ -1,11 +1,18 @@
-<div x-data="{ show: true }" x-show="show" x-transition
-     class="fixed top-4 right-4 z-[9999] w-full max-w-md">
+<div 
+    x-data="{ show: true }" 
+    x-show="show" 
+    x-transition
+    @keydown.escape.window="show = false"
+    x-init="$el.focus()"
+    tabindex="0"
+    class="fixed top-4 right-4 z-[9999] w-full max-w-md"
+>
     <div class="bg-red-50 border-l-4 border-red-500 text-red-700 p-4 rounded-lg shadow-lg flex items-start mx-4">
         <i class="ri-close-circle-fill text-xl mr-3"></i>
         <div class="flex-1">
             {{ $message ?? session('error') }}
         </div>
-        <button @click="show = false" class="text-red-600 hover:text-red-800 ml-2">
+        <button @click="show = false" class="text-red-600 hover:text-red-800 ml-2" aria-label="Dismiss">
             <i class="ri-close-line"></i>
         </button>
     </div>
